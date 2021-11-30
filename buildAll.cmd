656c90a96fc27b8a351b5cd9f0d6e86343aa4b5c
W0rlDx <michaelgmx1@googlemail.com>
2021-11-30 22:08:56 +0100

    Update buildAll script to use included cli

--- buildAll.cmd
+++ buildAll.cmd
@@ -23,23 +23,11 @@ dotnet tool uninstall ElectronNET.CLI -g
 dotnet tool install ElectronNET.CLI -g
 
 echo "/target xxx (dev-build)"
-electronize build /target custom win7-x86;win /dotnet-configuration Debug /electron-arch ia32  /electron-params "--publish never"
+..\ElectronNET.CLI\bin\Debug\net6.0\publish\dotnet-electronize-h5.exe build /target custom win7-x86;win /dotnet-configuration Debug /electron-arch ia32  /electron-params "--publish never"
 
 echo "/target win (dev-build)"
-electronize build /target win /electron-params "--publish never"
+..\ElectronNET.CLI\bin\Debug\net6.0\publish\dotnet-electronize-h5.exe build /target win /electron-params "--publish never"
 
 echo "/target custom win7-x86;win (dev-build)"
 
-electronize build /target custom win7-x86;win /electron-params "--publish never"
-
-:: Be aware, that for non-electronnet-dev environments the correct 
-:: invoke command would be dotnet electronize ...
-
-:: Not supported on Windows Systems, because of SymLinks...
-:: echo "/target osx"
-::   dotnet electronize build /target osx
-
-:: Linux and Mac is not supported on with this buildAll.cmd test file, because the electron bundler does some strange stuff
-:: Help welcome!
-:: echo "/target linux (dev-build)"
-:: electronize build /target linux /electron-params "--publish never"
+..\ElectronNET.CLI\bin\Debug\net6.0\publish\dotnet-electronize-h5.exe build /target custom win7-x86;win /electron-params "--publish never"
